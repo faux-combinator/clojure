ffb8b83a508a551e2f7f71a5a0840a119e070553
vendethiel <vendethiel@hotmail.fr>
2015-11-15 23:07:53 +0100

    small refactoring to lexer tests

--- test/faux_combinator/lexer_test.clj
+++ test/faux_combinator/lexer_test.clj
@@ -1,47 +1,48 @@
 (ns faux-combinator.lexer-test
-    (:require [clojure.test :refer :all]
-              [faux-combinator.lexer :refer [lex]]))
+  (:require [clojure.test :refer :all]
+            [faux-combinator.lexer :refer [lex]]))
+
+(def eq-token [:eq "="])
+(def dash-token [:dash "-"])
+(def under-token [:under "_"])
 
 (deftest lexer-basic-test
   (testing "it parses empty stuff"
-           (let [rules {}]
-                (is (= (lex rules "") []))
-                (is (= (lex rules "    ") []))))
+    (let [rules {}]
+      (is (= (lex rules "") []))
+      (is (= (lex rules "    ") []))))
 
   (testing "it parses basic stuff"
-           (let [rules {:eq "="}]
-                (is (= (lex rules "=") [[:eq "="]]))))
+    (let [rules {:eq "="}]
+      (is (= (lex rules "=") [[:eq "="]]))))
 
   (testing "it parses multiple tokens"
-           (let [rules {:eq "="}]
-                (is (= (lex rules "==") [[:eq "="] [:eq "="]]))))
+    (let [rules {:eq "="}]
+      (is (= (lex rules "==") [[:eq "="] [:eq "="]]))))
 
   (testing "it parses multiple, space-separated tokens"
-           (let [rules {:eq "="}]
-                (is (= (lex rules "=  =") [[:eq "="] [:eq "="]]))))
+    (let [rules {:eq "="}]
+      (is (= (lex rules "=  =") [[:eq "="] [:eq "="]]))))
   )
 
 (deftest lexer-multiple-rules-test
   ; XXX should these be `def`s?
-  (let [eq-token [:eq "="]
-        dash-token [:dash "-"]
-        under-token [:under "_"]
-        rules {:eq "=" :dash "-" :under "_"}]
-       (testing "it parses any rule"
-                (is (= (lex rules "=") [eq-token]))
-                (is (= (lex rules "-") [dash-token]))
-                (is (= (lex rules "_") [under-token]))
-                )
+  (let [rules {:eq "=" :dash "-" :under "_"}]
+    (testing "it parses any rule"
+      (is (= (lex rules "=") [eq-token]))
+      (is (= (lex rules "-") [dash-token]))
+      (is (= (lex rules "_") [under-token]))
+      )
 
-       (testing "it parses multiple rules"
-                (is (= (lex rules "=-_") [eq-token dash-token under-token]))
-                (is (= (lex rules "= -   _") [eq-token dash-token under-token])))
-       ))
+    (testing "it parses multiple rules"
+      (is (= (lex rules "=-_") [eq-token dash-token under-token]))
+      (is (= (lex rules "= -   _") [eq-token dash-token under-token])))
+    ))
 
 ; TODO (maybe) test/implement mutator form
 
 (deftest lexer-fails-test
   (testing "it correctly fails to parse invalid strings"
-           (let [rules {}]
-                (is (thrown? Exception (lex rules "anything")))))
+    (let [rules {}]
+      (is (thrown? Exception (lex rules "anything")))))
   )
