a19e2406fb0f4c3d7e550e6e28f3267f11da913f
vendethiel <vendethiel@hotmail.fr>
2015-11-15 23:08:12 +0100

    Implement more of the parser (one-of)

--- src/faux_combinator/parser.clj
+++ src/faux_combinator/parser.clj
@@ -1,6 +1,7 @@
 (ns faux-combinator.parser)
 
-(def ^:dynamic current-parser nil)
+(def ^:private ^:dynamic current-parser nil)
+(def mdr (+ 2 2))
 
 (defn do-parser
   "Returns a function that has a parser for the DSL-ish syntax.
@@ -8,12 +9,12 @@
   [parser]
   (fn [tokens]
     (dosync
-      (binding [current-parser (atom {:tokens tokens})]
-        (parser)))))
+     (binding [current-parser (atom {:tokens tokens})]
+       (parser)))))
 
 ;; extractors
-(def token-type first)
-(def token-value second)
+(def ^:private token-type first)
+(def ^:private token-value second)
 
 (defmacro make-parser
   "Simple wrapper around `do-parser` that thunks its rules"
@@ -24,8 +25,33 @@
   [expect-type]
   (let [token (first (:tokens @current-parser))]
     (if (= expect-type (token-type token))
-        (do
-          (swap! current-parser rest)
-          (token-value token))
-        (throw (Exception. (str "expected token " expect-type ", got "
-                                (token-type token)))))))
+      (do
+        (swap! current-parser rest)
+        (token-value token))
+      (throw (Exception. (str "expected token " expect-type ", got "
+                              (token-type token)))))))
+
+(defmacro maybe
+  "Simple thunking wrapper around `maybe-impl`"
+  [code]
+  `(maybe-impl (fn [] ~code)))
+
+(defn maybe-impl
+  "This tries to run a piece of match, and restores tokens if it didn't match"
+  [code]
+  (let [prev-tokens @current-parser]
+    (try
+      (code)
+      (catch Exception e
+        ; run for the hills! reset the parser!
+        (reset! current-parser prev-tokens)
+        false))))
+
+(defn one-of-impl [& code]
+  (or
+   (some maybe-impl code)
+   (throw (Exception. "No matching case of `one-of`"))))
+
+(defmacro one-of
+  [& code]
+  `(one-of-impl ~@(map #(list 'fn [] %) code)))
--- test/faux_combinator/parser_test.clj
+++ test/faux_combinator/parser_test.clj
@@ -3,17 +3,40 @@
             [faux-combinator.parser :refer :all]))
 
 (def eq-token [:eq "="])
-(def dash-token [:dash "="])
-(def under-token [:under "="])
-(def lparen-token [:lparen "="])
-(def rparen-token [:rparen "="])
+(def dash-token [:dash "-"])
+(def under-token [:under "_"])
+(def lparen-token [:lparen "("])
+(def rparen-token [:rparen ")"])
 
 (deftest parser-basic-test
   (testing "it can parse basic stuff"
-           (let [parser (make-parser
-                         (expect :eq)
-                         true)]
-                (is (= true (parser [eq-token])))
-             )))
+    (let [parser (make-parser
+                  (expect :eq)
+                  true)]
+      (is (= true (parser [eq-token])))))
+  (testing "can `maybe` match a token"
+    (let [parser (make-parser
+                  (maybe (expect :eq))
+                  true)]
+      (is (= true (parser [eq-token])))
+      (is (= true (parser [])))))
+  (testing "can `one-of` several alternatives"
+    (let [parser (make-parser
+                  (one-of (expect :eq)
+                          (expect :dash)
+                          (expect :under)))]
+      (is (= "=" (parser [eq-token])))
+      (is (= "-" (parser [dash-token])))
+      (is (= "_" (parser [under-token])))
+      (is (thrown? Exception (parser [])))
+      (is (thrown? Exception (parser [lparen-token])))
+      ))
+  )
 
-; TODO test for failure
+(deftest parser-fails-test
+  (testing "it correctly fails without the required component(s)"
+    (let [parser (make-parser
+                  (expect :anything)
+                  true)]
+      (is (thrown? Exception (parser [])))))
+  )
